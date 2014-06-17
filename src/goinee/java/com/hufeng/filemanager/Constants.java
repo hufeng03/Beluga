51d1b86ff820ff6cddc1bcfebe1c6e2d3a430829
Feng Hu <hufeng03@gmail.com>
2014-06-17 16:23:55 -0700

    add support for Gionee

---
+++ src/goinee/java/com/hufeng/filemanager/Constants.java
@@ -0,0 +1,16 @@
+package com.hufeng.filemanager;
+
+public class Constants {
+	public static final boolean RETAIN_FRAGMENT = false;
+	public static final boolean SHOW_AD = false;
+	public static final boolean USE_UMENG = false;
+    public static final boolean ENABLE_LANDSCAPE = false;
+    public static final boolean SHOW_KANBOX_UPLOAD_ACTION = true;
+    public static final boolean APP_RATE = false;
+    public static final boolean SHOW_SELECTED_CATEGORY = false;
+    public static final boolean SHOW_KANBOX_CATEGORY = true;
+    public static final String KANBOX_APK_URL="http://download.kanbox.com/kcn/android/kanbox_24101.apk"; //凡卓
+    public static final String CLIENT_ID = "a13c9cae4565783db3a50b8e9e73bfd6";		//kanbox sdk client_id
+    public static final String CLIENT_SECRET = "23c46d9be97bb25b1de62422b137aaa1";	//kanbox sdk client_secret
+    public static final String PRODUCT_FLAVOR_NAME = "goinee";
+}
