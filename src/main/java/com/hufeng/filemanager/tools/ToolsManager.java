728c0834dc0d172e6487fb2daa041190a29c3b30
Feng Hu <hufeng03@gmail.com>
2014-09-10 13:17:10 -0700

    add hosin partner

--- src/main/java/com/hufeng/filemanager/tools/ToolsManager.java
+++ src/main/java/com/hufeng/filemanager/tools/ToolsManager.java
@@ -23,11 +23,11 @@ public class ToolsManager {
 //			tools[2] = new Tool(R.string.tool_name_recommend, R.string.tool_description_recommend, R.drawable.tool_icn_http, "com.hufeng.filemanager",RECOMMEND);
 //		}
 		ArrayList<Tool> toolArray = new ArrayList<Tool>();
-		toolArray.add(new Tool(R.string.tool_name_ftp, R.string.tool_description_ftp, R.drawable.tool_icn_ftp, "com.hufeng.filemanager","com.hufeng.swiftp.ServerControlActivity"));
-		toolArray.add(new Tool(R.string.tool_name_http, R.string.tool_description_http, R.drawable.tool_icn_http, "com.hufeng.filemanager","com.hufeng.nanohttpd.ServerControlActivity"));
-    	toolArray.add(new Tool(R.string.tool_name_safe, R.string.tool_description_safe, R.drawable.tool_icn_safe, "com.hufeng.filemanager", "com.hufeng.filemanager.SafeBoxActivity"));
-		toolArray.add(new Tool(R.string.tool_name_kanbox, R.string.tool_description_kanbox, R.drawable.tool_icn_kanbox, "com.hufeng.filemanager", "com.hufeng.filemanager.KanBoxActivity"));
-        toolArray.add(new Tool(R.string.tool_name_selected, R.string.tool_description_selected, R.drawable.tool_icn_recommend, "com.hufeng.filemanager", "com.hufeng.filemanager.ResourceActivity"));
+		toolArray.add(new Tool(R.string.tool_name_ftp, R.string.tool_description_ftp, R.drawable.tool_icn_ftp, context.getPackageName(),"com.hufeng.swiftp.ServerControlActivity"));
+		toolArray.add(new Tool(R.string.tool_name_http, R.string.tool_description_http, R.drawable.tool_icn_http, context.getPackageName(),"com.hufeng.nanohttpd.ServerControlActivity"));
+    	toolArray.add(new Tool(R.string.tool_name_safe, R.string.tool_description_safe, R.drawable.tool_icn_safe, context.getPackageName(), "com.hufeng.filemanager.SafeBoxActivity"));
+		toolArray.add(new Tool(R.string.tool_name_kanbox, R.string.tool_description_kanbox, R.drawable.tool_icn_kanbox, context.getPackageName(), "com.hufeng.filemanager.KanBoxActivity"));
+        toolArray.add(new Tool(R.string.tool_name_selected, R.string.tool_description_selected, R.drawable.tool_icn_recommend, context.getPackageName(), "com.hufeng.filemanager.ResourceActivity"));
 
 //        toolArray.add(new Tool(R.string.tool_name_ftp, R.string.tool_description_ftp, R.drawable.tool_icn_ftp, "com.kanbox.filemanager","com.kanbox.swiftp.ServerControlActivity"));
 //        toolArray.add(new Tool(R.string.tool_name_http, R.string.tool_description_http, R.drawable.tool_icn_http, "com.kanbox.filemanager","com.kanbox.nanohttpd.ServerControlActivity"));
