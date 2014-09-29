9404be5c480319062b243dd7a6e94b9346a44484
Feng Hu <hufeng03@gmail.com>
2014-09-28 20:38:40 -0700

    fix crash bug for bodao

--- src/bodao/java/com/hufeng/filemanager/Constants.java
+++ src/bodao/java/com/hufeng/filemanager/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
     public static final boolean APP_RATE = false;
     public static final boolean SHOW_SELECTED_CATEGORY = false;
     public static final boolean SHOW_KANBOX_CATEGORY = true;
-    public static final String KANBOX_APK_URL=" http://download.kanbox.com/kcn/android/kanbox_21007.apk"; //波导
+    public static final String KANBOX_APK_URL="http://download.kanbox.com/kcn/android/kanbox_21007.apk"; //波导
     public static final String CLIENT_ID = "75240cd0e034568618cdfc416e787d66";		//kanbox sdk client_id
     public static final String CLIENT_SECRET = "f52d0af42c8919e8d7fc559b60db9938";	//kanbox sdk client_secret
     public static final String PRODUCT_FLAVOR_NAME = "bodao";
